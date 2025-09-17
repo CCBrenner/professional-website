875399ccb30dcd693ccd5344be64efe6870daf0f
Collyn Brenner <collynbrenner@gmail.com>
2025-09-17 14:18:52 -0500

    clearing away merge conflicts that got entered during mering of pull request

--- ProfessionalWebsite/Client/FEProjectControllers/HideAndSeekProject/Opponent.cs
+++ ProfessionalWebsite/Client/FEProjectControllers/HideAndSeekProject/Opponent.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Diagnostics;
 
 namespace ProfessionalWebsite.Client.FEProjectControllers.HideAndSeekProject;
@@ -38,40 +37,3 @@ public class Opponent
             $"in the {currentLocation.Name}");
     }
 }
-=======
-﻿using ProfessionalWebsite.Client.ProjAssets.HideAndSeekProject;
-using System.Diagnostics;
-
-namespace ProfessionalWebsite.Client.ProjAssets.HideAndSeekProject;
-
-public class Opponent
-{
-    public Opponent(string name) => Name = name;
-    public readonly string Name;
-    public override string ToString() => Name;
-    public void Hide()
-    {
-        Location currentLocation = House.Entry;
-        int locationsToMoveThrough = House.Random.Next(10, 20);
-
-        for (int i = 0; i < locationsToMoveThrough; i++)
-            currentLocation = House.RandomExit(currentLocation);
-        while (true)
-        {
-            if (currentLocation.GetType() == typeof(LocationWithHidingPlace))
-            {
-                ((LocationWithHidingPlace)currentLocation).Hide(this);
-                break;
-            }
-            else
-            {
-                currentLocation = House.RandomExit(currentLocation);
-            }
-        }
-
-        Debug.WriteLine($"{Name} is hiding " +
-            $"{(currentLocation as LocationWithHidingPlace).HidingPlaceName} " +
-            $"in the {currentLocation.Name}");
-    }
-}
->>>>>>> dd78908e3871e0915f6cfcde1e4391618ac793a1
